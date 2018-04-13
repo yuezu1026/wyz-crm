dd2affcf48023b48907b8c4d0827546307b16e94
yuezu1026 <39002818@qq.com>
2018-04-13 13:56:54 +0800

    add

--- svc-login-reg-service/src/main/java/service/rest/TestController.java
+++ svc-login-reg-service/src/main/java/service/rest/TestController.java
@@ -1,10 +1,12 @@
 package service.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  
+@RestController
+@RefreshScope
 class TestController {  
       
     @Value("${wayne.from}")  
