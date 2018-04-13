9c11b6c2ad5788c9feddc0385a21e2027a615263
yuezu1026 <39002818@qq.com>
2018-04-13 13:05:18 +0800

    add

---
+++ svc-account/src/main/java/service/AccountApplication.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableHystrix
+public class AccountApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+}
